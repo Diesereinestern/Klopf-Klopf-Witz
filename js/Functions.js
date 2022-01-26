d59170609357d0fa5b94fc0abe5c631aedcff91f
Star.- <lennartsojka@gmail.com>
2022-01-26 23:40:08 +0100

    Added clipboard functions and new stuff for an experimental sidebar

--- js/Functions.js
+++ js/Functions.js
@@ -13,3 +13,23 @@
             console.log("Reload abgebrochen")
           }
       }
+
+    function copySiteLink ()
+     {
+       navigator.clipboard.writeText(window.location.href);
+     }
+
+    function copyDcName ()
+     {
+       navigator.clipboard.writeText('Star.-.#2795');
+     }
+
+    function w3_open()
+     {
+       document.getElementById("mySidebar").style.display = "block";
+     }
+
+    function w3_close()
+     {
+       document.getElementById("mySidebar").style.display = "none";
+     }
