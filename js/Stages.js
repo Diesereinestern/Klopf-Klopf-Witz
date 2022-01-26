3310adfacb29d371b266e8318ee892ee156200ee
Star.- <lennartsojka@gmail.com>
2022-01-26 23:39:09 +0100

    Reformatting

--- js/Stages.js
+++ js/Stages.js
@@ -2,22 +2,21 @@
 
 
 
-function stage1 ()
-{
-  document.getElementById("Placeholder1").innerHTML = "Klopf Klopf <br>"
-  var btn2 = addButton("Wer ist da?","button","btn2","Placeholder2");
-  document.getElementById("btn2").addEventListener('click', stage2());
+    function stage1 ()
+     {
+       document.getElementById('Placeholder1').innerHTML = "Klopf Klopf <br>"
+       var btn2 = addButton("Wer ist da?","button",'btn2',"Placeholder2");
+       document.getElementById('btn2').addEventListener('click', stage2());
+     }
 
-}
+    function stage2 ()
+     {
+      alert("Stage 2 Alert")
+      removeButton('btn1')
 
-function stage2 ()
-{
-  document.getElementById("btn1").disabled = true;
-  var button = document.getElementById("btn1");
-  button.remove();                              
-}
+     }
 
-function stage3 ()
-{
-  removeButton('btn1')
-}
+    function stage3 ()
+     {
+       alert("Stage 3 Alert")
+     }
